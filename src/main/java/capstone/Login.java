906ad687f07239b0182a4d61165e9164d3c18dfd
R93_Sniper <wahab_q@msn.com>
2022-09-24 14:44:05 -0400

    Basic Login Screen frontend

---
+++ src/main/java/capstone/Login.java
@@ -0,0 +1,14 @@
+package capstone;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+/**
+ *  Login Class
+ * 
+ * Driver Class object for login screen
+ * @author Wahab Quazi
+ */
+public class Login {
+    
+}
