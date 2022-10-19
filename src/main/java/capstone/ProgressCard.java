407048f758f8d68aff799cca9d72d9b3bc7d8525
nka2001 <89935053+nka2001@users.noreply.github.com>
2022-10-18 21:50:25 -0400

    Validated Data Entered by user

--- src/main/java/capstone/ProgressCard.java
+++ src/main/java/capstone/ProgressCard.java
@@ -6,7 +6,12 @@
 package capstone;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
@@ -30,9 +35,67 @@ public class ProgressCard {
     We need to set the currentDate on initialize of this controller
     I forgot what the method name was.
     Be sure that we grab the current system date
-    */
+     */
+    private DateTimeFormatter dt = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+    private LocalDateTime now = LocalDateTime.now();
+    @FXML
+    private Button measureMe;
+
+    /**
+     * intialize method, calls upon opening this controller sets the date to the
+     * current system date
+     */
+    public void initialize() {
+
+        currentDate.setText(dt.format(now));
+
+    }
+
+    /**
+     * method to validate data collected from the user
+     */
+    public void validateEntry() {
+
+        if (currentWeight.getText().equals("")) {//if the person enters nothing
+            makeAlert("Error, you did enter a weight");
+        }
+
+        if (rb_pounds.isSelected()) {//if pounds is selected then validate data in pounds
+
+            if (Double.parseDouble(currentWeight.getText()) <= 0) {//if the person enters a negative weight or 0
+                makeAlert("Error, you entered a negative weight");
+            } else if (Double.parseDouble(currentWeight.getText()) > 1000) {//if the person enters over 1000 pounds
+                makeAlert("Error, you enteted a weight over 1000lbs");
+            }
+        } else if (rb_kilo.isSelected()) {//if KG is selected then validate the data in kilograms
+               
+            if (Double.parseDouble(currentWeight.getText()) <= 0) {//if the person enters a negative weight or 0
+                makeAlert("Error, you entered a negative weight");
+            } else if (Double.parseDouble(currentWeight.getText()) > 453) {//if the person enters over 1000 pounds (in kg)
+                makeAlert("Error, you enteted a weight over 453 kgs");
+
+            }
+        } else {//unknwon/reserved for future use
+            makeAlert("Error, not sure how you got here but good job");
+        }
+    }
+        
     
-    
+
+    /**
+     * method to make an alert for entering invalid data
+     *
+     * @param alertText
+     */
+    public void makeAlert(String alertText) {
+
+        Alert a = new Alert(AlertType.ERROR);
+        a.setTitle("Error");
+        a.setHeaderText(alertText);
+        a.showAndWait();
+
+    }
+
     @FXML
     public void goBack() throws IOException {
         App.setRoot("UserHome");
@@ -40,11 +103,15 @@ public class ProgressCard {
 
     @FXML
     public void saveCard() throws IOException {
+        
+      
+        validateEntry();
         //TODO: Make this save the given contents to DB
     }
 
     @FXML
     public void goToMeasure() throws IOException {
+        makeAlert("Error, feature not yet implemented");
         //App.setRoot("MeasureCard");
     }
 
@@ -55,6 +122,7 @@ public class ProgressCard {
 
     @FXML
     public void goToExercise() throws IOException {
+        makeAlert("Error feature not yet implemented");
         //App.setRoot("ExerciseCard");
     }
 }
